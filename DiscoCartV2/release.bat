a72c309f268ddd4c872a317d92b9c867de5f4d38
Christopher Mallery <zoggins@retro-spy.com>
2025-05-13 22:32:00 -0700

    Updating path to codesign tool

--- DiscoCartV2/release.bat
+++ DiscoCartV2/release.bat
@@ -25,7 +25,7 @@ cd "bin\Release\net8.0\"
 if %ERRORLEVEL% NEQ 0 goto :fail
 
 if exist "..\..\..\..\..\..\..\certs\codesign.cer" (
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x86\SignTool" sign /f "..\..\..\..\..\..\..\certs\codesign.cer" /csp "eToken Base Cryptographic Provider" /k "%codesignpasswd%" /tr http://timestamp.comodoca.com  /td sha256 /fd sha256 /a DiscoCartUtil.exe
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x86\SignTool" sign /f "..\..\..\..\..\..\..\certs\codesign.cer" /csp "eToken Base Cryptographic Provider" /k "%codesignpasswd%" /tr http://timestamp.comodoca.com  /td sha256 /fd sha256 /a DiscoCartUtil.exe
 if %ERRORLEVEL% NEQ 0 goto :fail
 )
 
@@ -51,7 +51,7 @@ if exist "C:\Program Files (x86)\Actual Installer\actinst.exe" (
 "C:\Program Files (x86)\Actual Installer\actinst.exe" /S ".\DiscoCartUtil-64.aip"
 if %ERRORLEVEL% NEQ 0 goto :fail
   if exist "..\..\..\..\certs\codesign.cer" (
-    "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x86\SignTool" sign /f "..\..\..\..\certs\codesign.cer" /csp "eToken Base Cryptographic Provider" /k "%codesignpasswd%" /tr http://timestamp.comodoca.com  /td sha256 /fd sha256 /a DiscoCartUtil-Setup.exe
+    "C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x86\SignTool" sign /f "..\..\..\..\certs\codesign.cer" /csp "eToken Base Cryptographic Provider" /k "%codesignpasswd%" /tr http://timestamp.comodoca.com  /td sha256 /fd sha256 /a DiscoCartUtil-Setup.exe
     if %ERRORLEVEL% NEQ 0 goto :fail
   )
 )
